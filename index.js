d5439393fdba2103c18c6f206c722b30cd4b9f51
Shivanshu666 <shivanshuofficial123@gmail.com>
2025-08-03 18:06:36 -0500

    File Change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config();
 const mongoose = require("mongoose");
 
-const userRouter = require("./app/Router/userRoute");
+const userRouter = require("./app/Router/userRoute-temp");
 const jobsRouter = require("./app/Router/jobsRoute");
 const contactRouter = require("./app/Router/contactRoute");
 const authRoutes = require("./app/Router/authRoutes");
